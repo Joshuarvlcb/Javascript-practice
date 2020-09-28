49c5428be35d35db7450804fe8eb9edf80932302
josh <joshuarvlcb@gmail.com>
2020-09-27 19:01:51 -0700

    adding this so i can practice coding at my sisters house

--- script.js
+++ script.js
@@ -90,14 +90,7 @@ var $years =3;
 // console.log(x);
 // x += 10
 
-// var markHeight, markMass, johnHeight, johnMass;
-// markHeight = 1.8;//meters
-// johnHeight =1.4;//meters
-// markMass =   71 ;// mass in kg
-// johnMass =   60;// mass in kg
 
-// var markBMI = markMass / (markHeight*markHeight);
-// var johnBMI =johnMass / (johnHeight*johnHeight);
 
 // var greaterBMI = markBMI < johnBMI;
 
@@ -267,7 +260,7 @@ var $years =3;
 
 // function yearsUntilRetirement(year, firstName){
 //     var age = calculateAge(year);
-//     var retirement =65-age;
+//     var retirement = 65-age;
 
 //     if (retirement > 0){
 //         console.log(firstName + ' retires in ' + retirement + ' years');
@@ -317,7 +310,7 @@ var $years =3;
 // console.log(names)
 
 // //diferent data types
-// var john = ['John','Smith',1890,'teacher',false,'designer'];
+// ll
 
 // john.push('blue');
 // john.unshift('Mr.')
@@ -327,8 +320,320 @@ var $years =3;
 // var isDesigner = john.indexOf('designer') === -1 ? 'john is not a designer': 'john is a designer';
 // console.log(isDesigner);
 
-var johnBills = [124,48,268]
-function johnBillsTips (tips,bills){
-    console.log(johnBills[0]*0.15)
+
+//chalenge 2
+
+// function tipCalculator (bill){
+// var tips ;
+
+// if (bill < 50){
+//     tips = .20
+// }else if (bill >=50 && bill < 200){
+//     tips= .15
+// }else {
+//     tips = .10
+// }
+// return bill*tips
+// }
+// console.log(tipCalculator(21));
+
+// var bills =[124,48,220]
+
+// var tips = [tipCalculator(124),tipCalculator(48),tipCalculator(220)]
+// var tipsAndBills = [tips[0] + bills[0],tips[1] + bills[1],tips[2] + bills[2]];
+
+// console.log(tips);
+// console.log(tipsAndBills);
+
+
+
+
+
+
+
+
+// tax calculator
+
+// var stateTaxes = ['california','nevada','arizona']
+
+// function federalTaxCalculator (taxes){
+//     var taxPercent;
+//     if (taxes< 40000){
+//         taxPercent =.12
+//     }
+//     else if (taxes >= 39000 && taxes < 84000){
+//         taxPercent =.22
+
+//     }else if (taxes>=84000 && taxes <160000){
+//         taxPercent =.24
+
+//     }else if (taxes >= 160000 && taxes < 204000){
+//         taxPercent = .32
+
+//     }else{
+//         taxPercent = .37
+//     }
+//     return taxes*taxPercent
+// }
+// var incomeTax = [70000,35000,550000,]
+
+// var tax = [federalTaxCalculator(incomeTax[0])
+// ,federalTaxCalculator(incomeTax[1])
+// ,federalTaxCalculator(incomeTax[2])];
+// console.log(tax);
+
+// var taxPlusIncome = [
+//     tax[0] + incomeTax[0],
+//     tax[1] + incomeTax[1],
+//     tax[2] + incomeTax[2]];
+
+// console.log(taxPlusIncome)
+
+//objects and properties
+
+//objects can group 2 variables togerther that dont have a particulor order similar to arrays
+
+// var john = {
+//     firstName: 'john',
+//     lastName: 'smith',
+//     birthYear: 1990,
+//     family:['jane','mark','emily','bob'],
+//     job:'teacher',
+//     isMarried:false
+// };
+// console.log(john.firstName);
+// console.log(john['lastName']);
+// var x = 'lastName'
+// console.log(john[x]);
+
+// john.job = 'soccer player';
+// john['isMarried'] = true;
+// console.log(john);
+
+// var jane = new Object();
+// jane.name = 'jane';
+// jane.birthday = 2003;
+// jane.profession = 'Tennis Player';
+// jane['lastName'] = 'Smithy';
+// console.log(jane);
+
+//object methods can even attach functions
+
+// var john = {
+//     firstName: "john",
+//     lastName: "smith",
+//     birthYear: 1990,
+//     family: ["jane", "mark", "emily", "bob"],
+//     job: "teacher",
+//     year: 2030,
+//     isMarried: false,
+//     calcAge: function(){
+//         this.age = 2020 - this.birthYear
+//     }
+// };
+
+// john.calcAge()
+// console.log(john);
+
+
+// var john = {
+//     firstName: 'john',
+//     lastName: 'Mcfarland',
+//     height:1.2, //meters
+//     mass:80, //kilograms
+//     bmiCalc: function(){
+//        this.bmi = this.mass/(this.height * this.height)
+//     }
+// };
+
+// john.bmiCalc();
+// console.log(john);
+
+// var mark = {
+//     firstName: 'Mark',
+//     lastName: 'Tate',
+//     height: 1.2, //meters
+//     mass: 80, //kilograms
+//     bmiCalc: function(){
+//         this.bmi = this.mass/(this.height * this.height)
+//     }
+// };
+// mark.bmiCalc();
+// console.log(mark,mark.bmiCalc,john.bmiCalc);
+
+// if (mark.bmi > john.bmi){
+//     console.log( mark.firstName + ' ' + mark.lastName + ' has the highest BMI');
+// }else if (mark.bmi < john.bmi){
+//     console.log(john.firstName + ' ' + john.lastName + " has the highest BMI");
+// }else{
+//     console.log( 'same bmi');
+// }
+
+
+
+// var markHeight, markMass, johnHeight, johnMass;
+// markHeight = 1.8;//meters
+// johnHeight =1.4;//meters
+// markMass =   71 ;// mass in kg
+// johnMass =   60;// mass in kg
+
+// var markBMI = markMass / (markHeight*markHeight);
+// var johnBMI =johnMass / (johnHeight*johnHeight);
+
+// loops and iteration
+// lllll
+
+// i = 0, 0 < 10 true, log i to console, i++
+//i = 1,1 , 10 true, log i to the console, i++
+// ...
+// i = 9, 9 < 10 true, log i to the console, i++
+//i = 10, 10 < 10 FALSE, exit the loop!
+
+// var john = ["John", "Smith", 1890, "teacher", false, "designer"];
+
+// for(var i = 0; i < john.length; i++){
+//     console.log(john[i]);
+// }
+
+
+// // while loop
+// var i = 0
+// while(i < john.length){
+//     console.log(john[1]);
+//     i++
+// }
+
+//continue and break statements
+
+// var john = ["John", "Smith", 1890, "teacher", false, "designer"];
+
+// for(var i = 0; i < john.length; i++){
+//     if(typeof john[i] !== 'string') continue;
+//     console.log(john[i]);
+// }
+
+// for (var i = 0; i < john.length; i++) {
+//     if (typeof john[i] !== "string") break;
+//     console.log(john[i]);
+// }
+
+// for (var i = 6 ; i < john.length; i--) {
+//     if (typeof john[i] !== "string") break;
+//     console.log(john[i]);
+
+
+//looping backwards
+// for (var i = john.length -1 ; i >=0; i--){
+//     console.log(john[i]);
+// }
+
+var john = {
+    bills:[124,48,268,180,42],
+    tipsCalc:function (){
+        this.tip = [];
+        this.finalvalues = [];
+
+        for (var i =0; i < this.bills.length; i++){
+            
+            // Determine percentage nased on tippign rules
+            var percentage;
+            var bill = this.bills[i]
+
+            if (bill < 50){
+                percentage =.20;
+            }else if (bill >= 50 && bill < 200){
+                percentage =.15;
+            }else{
+                percentage =.10;
+            }
+            
+
+
+            //adding results
+            this.tip[i] = bill * percentage;
+            this.finalvalues[i] = bill + bill * percentage;
+        }
+    }
+
+    }
+
+    
+var tips= []
+function markTip (){
+    var bills = [77,375,110,45];
+    var percent;
+    
+    for(var i = 0;i < bills.length; i++){
+
+        var bill = bills.length[i];
+
+        if (bill < 100 ){
+            percent =.20;
+        }else if (bill >= 100 && bill <300){
+            percent =.10
+        }else{
+            percent =.25
+        }
+
+
+    }
+        return tips = bill*percent;
+
 }
-johnBillsTips();
+markTip()
+console.log(markTip)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+john.tipsCalc()
+console.log(john);
+
+
+
+var joshua = {
+    fullName: 'Joshua Ruvalcaba',
+    age: [17,87,23,45,67,89],
+    birthYear:function (){
+        this.birthYear = [];
+        
+        for (var i = 0;i < this.age.length; i++){
+
+            var age = this.age.length[i];
+            var year;
+
+
+            if (age < 50) {
+            year = 2020;
+            } else if (age >= 50 && age < 200) {
+            year = 2020;
+            } else {
+            year = 2020;
+            }
+            
+
+
+            this.birthYear[i] = Number(age - year);
+    }
+        }
+
+        
+
+};
+
+joshua.birthYear();
+console.log(joshua);
+
